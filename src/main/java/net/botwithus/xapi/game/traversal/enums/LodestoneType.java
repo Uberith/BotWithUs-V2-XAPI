ff49be6d1717ff1e805ff6d06dfa67ff13177403
Lewis Matlock <36051087+Uberith@users.noreply.github.com>
2025-09-25 20:08:11 -0700

    Local Commit

--- src/main/java/net/botwithus/xapi/game/traversal/enums/LodestoneType.java
+++ src/main/java/net/botwithus/xapi/game/traversal/enums/LodestoneType.java
@@ -10,44 +10,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public enum LodestoneType {
-    AL_KHARID(11, 28),
-    ANACHRONIA(25, 44270),
-    ARDOUGNE(12, 29),
-    ASHDALE(34, 22430),
-    BANDIT_CAMP(9, 9482),
-    BURTHORPE(13, 30),
-    CANIFIS(27, 18523),
-    CATHERBY(14, 31),
-    CITY_OF_UM(36, 53270),
-    DRAYNOR_VILLAGE(15, 32),
-    EDGEVILLE(16, 33),
-    EAGLES_PEAK(28, 18524),
-    FALADOR(17, 34),
-    FORT_FORINTHRY(23, 52518),
-    FREMENNIK_PROVINCE(29, 18525),
-    KARAMJA(30, 18526),
-    LUMBRIDGE(18, 35),
-    LUNAR_ISLE(10, 9482),
-    MENAPHOS(24, 36173),
-    OOGLOG(31, 18527),
-    PORT_SARIM(19, 36),
-    PRIFDDINAS(35, 24967),
-    SEERS_VILLAGE(20, 37),
-    TAVERLY(21, 38),
-    TIRANNWN(32, 18528),
-    VARROCK(22, 39),
-    WILDERNESS_CRATER(33, 18529),
-    YANILLE(26, 40);
+    AL_KHARID(11, "Al Kharid"),
+    ANACHRONIA(25, "Anachronia"),
+    ARDOUGNE(12, "Ardougne"),
+    ASHDALE(34, "Ashdale"),
+    BANDIT_CAMP(9, "Bandit Camp"),
+    BURTHORPE(13, "Burthorpe"),
+    CANIFIS(27, "Canifis"),
+    CATHERBY(14, "Catherby"),
+    CITY_OF_UM(36, "City of Um"),
+    DRAYNOR_VILLAGE(15, "Draynor Village"),
+    EDGEVILLE(16, "Edgeville"),
+    EAGLES_PEAK(28, "Eagles' Peak"),
+    FALADOR(17, "Falador"),
+    FORT_FORINTHRY(23, "Fort Forinthry"),
+    FREMENNIK_PROVINCE(29, "Fremennik Province"),
+    KARAMJA(30, "Karamja"),
+    LUMBRIDGE(18, "Lumbridge"),
+    LUNAR_ISLE(10, "Lunar Isle"),
+    MENAPHOS(24, "Menaphos"),
+    OOGLOG(31, "Oo'glog"),
+    PORT_SARIM(19, "Port Sarim"),
+    PRIFDDINAS(35, "Prifddinas"),
+    SEERS_VILLAGE(20, "Seers' Village"),
+    TAVERLY(21, "Taverley"),
+    TIRANNWN(32, "Tirannwn"),
+    VARROCK(22, "Varrock"),
+    WILDERNESS_CRATER(33, "Wilderness Crater"),
+    YANILLE(26, "Yanille");
 
     private static final Logger logger = LoggerFactory.getLogger(LodestoneType.class);
     private static final int INTERFACE_ID = 1092;
 
     private final int componentId;
-    private final int varbitId;
+    private final String teleportAction;
 
-    LodestoneType(int componentId, int varbitId) {
+    LodestoneType(int componentId, String teleportTarget) {
         this.componentId = componentId;
-        this.varbitId = varbitId;
+        if (teleportTarget == null || teleportTarget.isBlank()) {
+            this.teleportAction = null;
+        } else {
+            this.teleportAction = "Teleport " + teleportTarget;
+        }
     }
 
     public boolean teleport(DelayableScript script) {
@@ -57,6 +61,7 @@ public enum LodestoneType {
             logger.warn("Cannot teleport via {} because the local player is null", this);
             return false;
         }
+
         script.delay(Rand.nextInt(4500, 6500));
 
         if (!LodestoneNetwork.isOpen()) {
@@ -65,48 +70,49 @@ public enum LodestoneType {
                 logger.warn("Failed to open the Lodestone network interface for {}", this);
                 return false;
             }
-            script.delay(Rand.nextInt(4500, 6500));
+            if (!LodestoneNetwork.isOpen()) {
+                int waitTicks = Rand.nextInt(18, 26);
+                logger.debug("Waiting up to {} ticks for the Lodestone network interface to open for {}", waitTicks, this);
+                script.delayUntil(LodestoneNetwork::isOpen, waitTicks);
+                return false;
+            }
         }
 
+        logger.debug("Lodestone network interface open; locating component for {}", this);
         var componentId = this.componentId;
         var component = Interfaces.getComponent(INTERFACE_ID, componentId);
         if (component == null) {
-            logger.warn("Component lookup failed for {} (interfaceId={}, componentId={})", this, INTERFACE_ID, componentId);
+            logger.debug("Teleport component for {} not yet available (interfaceId={}, componentId={}); delaying before retrying", this, INTERFACE_ID, componentId);
+            script.delay(Rand.nextInt(8, 12));
             return false;
         }
 
-        var interactionResult = component.interact("Teleport");
-        if (interactionResult > 0) {
-            int wax = VarDomain.getVarBitValue(28623);
-            int quick = VarDomain.getVarBitValue(28622);
-            if (logger.isDebugEnabled()) {
-                logger.debug("Teleport interaction succeeded for {} (quick={}, wax={})", this, quick, wax);
-            }
-            if (quick == 1 && wax > 0) {
-                script.delay(Rand.nextInt(4500, 6500));
-            } else {
-                script.delay(Rand.nextInt(12000, 14000));
-            }
-            logger.info("Teleport initiated for {}", this);
-            return true;
+        int interactionResult = component.interact("Teleport");
+        if (interactionResult <= 0 && teleportAction != null) {
+            interactionResult = component.interact(teleportAction);
+        }
+        if (interactionResult <= 0) {
+            interactionResult = component.interact();
+        }
+        if (interactionResult <= 0) {
+            logger.warn("Failed to interact with {} (interaction result: {}). Retrying after short delay.", this, interactionResult);
+            script.delay(Rand.nextInt(10, 16));
+            return false;
         }
 
-        logger.warn("Failed to interact with {} (interaction result: {})", this, interactionResult);
-        return false;
-    }
-
-    public boolean isAvailable() {
-        var result = VarDomain.getVarBitValue(varbitId);
-        boolean available;
-        switch (this) {
-            case LUNAR_ISLE -> available = result >= 100;
-            case BANDIT_CAMP -> available = result >= 15;
-            default -> available = result == 1;
-        }
+        int wax = VarDomain.getVarBitValue(28623);
+        int quick = VarDomain.getVarBitValue(28622);
         if (logger.isDebugEnabled()) {
-            logger.debug("Availability check for {} returned {} (varbitId={}, value={})", this, available, varbitId, result);
+            logger.debug("Teleport interaction succeeded for {} (quick={}, wax={})", this, quick, wax);
         }
-        return available;
+        if (quick == 1 && wax > 0) {
+            script.delay(Rand.nextInt(4500, 6500));
+        } else {
+            script.delay(Rand.nextInt(12000, 14000));
+        }
+        logger.info("Teleport initiated for {}", this);
+        return true;
     }
+
 }
 
