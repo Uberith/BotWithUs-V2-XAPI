4ab433b9417d587a531d44fa5ae7bf2dccf2d612
Lewis Matlock <36051087+Uberith@users.noreply.github.com>
2025-09-25 20:24:39 -0700

    Local Commit

--- src/main/java/net/botwithus/xapi/game/traversal/enums/LodestoneType.java
+++ src/main/java/net/botwithus/xapi/game/traversal/enums/LodestoneType.java
@@ -10,48 +10,71 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public enum LodestoneType {
-    AL_KHARID(11, "Al Kharid"),
-    ANACHRONIA(25, "Anachronia"),
-    ARDOUGNE(12, "Ardougne"),
-    ASHDALE(34, "Ashdale"),
-    BANDIT_CAMP(9, "Bandit Camp"),
-    BURTHORPE(13, "Burthorpe"),
-    CANIFIS(27, "Canifis"),
-    CATHERBY(14, "Catherby"),
-    CITY_OF_UM(36, "City of Um"),
-    DRAYNOR_VILLAGE(15, "Draynor Village"),
-    EDGEVILLE(16, "Edgeville"),
-    EAGLES_PEAK(28, "Eagles' Peak"),
-    FALADOR(17, "Falador"),
-    FORT_FORINTHRY(23, "Fort Forinthry"),
-    FREMENNIK_PROVINCE(29, "Fremennik Province"),
-    KARAMJA(30, "Karamja"),
-    LUMBRIDGE(18, "Lumbridge"),
-    LUNAR_ISLE(10, "Lunar Isle"),
-    MENAPHOS(24, "Menaphos"),
-    OOGLOG(31, "Oo'glog"),
-    PORT_SARIM(19, "Port Sarim"),
-    PRIFDDINAS(35, "Prifddinas"),
-    SEERS_VILLAGE(20, "Seers' Village"),
-    TAVERLY(21, "Taverley"),
-    TIRANNWN(32, "Tirannwn"),
-    VARROCK(22, "Varrock"),
-    WILDERNESS_CRATER(33, "Wilderness Crater"),
-    YANILLE(26, "Yanille");
+    AL_KHARID(11, 28, "Al Kharid"),
+    ANACHRONIA(25, 44270, "Anachronia"),
+    ARDOUGNE(12, 29, "Ardougne"),
+    ASHDALE(34, 22430, "Ashdale"),
+    BANDIT_CAMP(9, 9482, "Bandit Camp"),
+    BURTHORPE(13, 30, "Burthorpe"),
+    CANIFIS(27, 18523, "Canifis"),
+    CATHERBY(14, 31, "Catherby"),
+    CITY_OF_UM(36, 53270, "City of Um"),
+    DRAYNOR_VILLAGE(15, 32, "Draynor Village"),
+    EDGEVILLE(16, 33, "Edgeville"),
+    EAGLES_PEAK(28, 18524, "Eagles' Peak"),
+    FALADOR(17, 34, "Falador"),
+    FORT_FORINTHRY(23, 52518, "Fort Forinthry"),
+    FREMENNIK_PROVINCE(29, 18525, "Fremennik Province"),
+    KARAMJA(30, 18526, "Karamja"),
+    LUMBRIDGE(18, 35, "Lumbridge"),
+    LUNAR_ISLE(10, 9482, "Lunar Isle"),
+    MENAPHOS(24, 36173, "Menaphos"),
+    OOGLOG(31, 18527, "Oo'glog"),
+    PORT_SARIM(19, 36, "Port Sarim"),
+    PRIFDDINAS(35, 24967, "Prifddinas"),
+    SEERS_VILLAGE(20, 37, "Seers' Village"),
+    TAVERLY(21, 38, "Taverley"),
+    TIRANNWN(32, 18528, "Tirannwn"),
+    VARROCK(22, 39, "Varrock"),
+    WILDERNESS_CRATER(33, 18529, "Wilderness Crater"),
+    YANILLE(26, 40, "Yanille");
 
     private static final Logger logger = LoggerFactory.getLogger(LodestoneType.class);
     private static final int INTERFACE_ID = 1092;
 
     private final int componentId;
+    private final int varbitId;
     private final String teleportAction;
 
-    LodestoneType(int componentId, String teleportTarget) {
+    LodestoneType(int componentId, int varbitId, String teleportTarget) {
         this.componentId = componentId;
+        this.varbitId = varbitId;
         if (teleportTarget == null || teleportTarget.isBlank()) {
             this.teleportAction = null;
         } else {
             this.teleportAction = "Teleport " + teleportTarget;
         }
+    }
+
+    public boolean isAvailable() {
+        int result = VarDomain.getVarBitValue(varbitId);
+        if (logger.isDebugEnabled()) {
+            logger.debug("Availability check for {} returned {} (varbit={})", this, result, varbitId);
+        }
+        switch (this) {
+            case LUNAR_ISLE -> {
+                return result >= 100;
+            }
+            case BANDIT_CAMP -> {
+                return result >= 15;
+            }
+        }
+        return result == 1;
+    }
+
+    @Deprecated(forRemoval = false)
+    public boolean isAvaialable() {
+        return isAvailable();
     }
 
     public boolean teleport(DelayableScript script) {
